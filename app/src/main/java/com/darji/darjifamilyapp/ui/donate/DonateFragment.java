a98e53f6553d685b156cfe7f8aa8f0d3c7e18771
Jeet <jeet.glsica17@gmail.com>
2020-08-04 15:20:55 +0530

    Filtering Jobs

--- app/src/main/java/com/darji/darjifamilyapp/ui/donate/DonateFragment.java
+++ app/src/main/java/com/darji/darjifamilyapp/ui/donate/DonateFragment.java
@@ -4,10 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import com.darji.darjifamilyapp.R;
 
 public class DonateFragment extends Fragment {
--- app/src/main/java/com/darji/darjifamilyapp/ui/jobs/JobsFragment.java
+++ app/src/main/java/com/darji/darjifamilyapp/ui/jobs/JobsFragment.java
@@ -1,13 +1,18 @@
 package com.darji.darjifamilyapp.ui.jobs;
 
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -31,6 +36,11 @@ public class JobsFragment extends Fragment {
     RecyclerView jobs;
     JobsAdapter jobsAdapter;
     List<JobsData> joblist;
+
+    ConstraintLayout expandableView;
+    Button arrowBtn;
+    CardView cardView;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
@@ -41,6 +51,26 @@ public class JobsFragment extends Fragment {
         jobs = root.findViewById(R.id.jobs);
         jobs.setHasFixedSize(true);
         jobs.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        expandableView = root.findViewById(R.id.expandableView);
+        arrowBtn = root.findViewById(R.id.arrowBtn);
+        cardView = root.findViewById(R.id.cardView);
+
+        arrowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (expandableView.getVisibility()==View.GONE){
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    expandableView.setVisibility(View.VISIBLE);
+                    arrowBtn.setBackgroundResource(R.drawable.ic_launcher_foreground);
+                } else {
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    expandableView.setVisibility(View.GONE);
+                    arrowBtn.setBackgroundResource(R.drawable.ic_launcher_foreground);
+                }
+            }
+        });
+
 
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
         Call<List<JobsData>> data = apiService.getJobs();
